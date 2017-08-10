fd72ab7a5e9d20f98ffb4c71e6aa39bcd498ac04
Rodel-Ubuntu <rodelcruz.316@gmail.com>
2017-08-11 00:47:00 +0800

    Trim dependencies and their initialization

--- app.js
+++ app.js
@@ -11,11 +11,6 @@ var index = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-// app.set('view engine', 'handlebars');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
